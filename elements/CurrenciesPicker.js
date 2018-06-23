0781ff81304fff55021459da3577a2f5676cc7be
kamilnaja <kamilnaja@gmail.com>
2018-06-23 18:58:00 +0200

    separe picker

---
+++ elements/CurrenciesPicker.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Picker,
+} from 'react-native';
+import { styles } from './../styles/style';
+import { currencies } from './../currencies';
+
+export default class CurrenciesPicker extends Component {
+    displayCurrencies() {
+        return currencies.map((item, id) => <Picker.Item label={item.label} value={item.value} key={id}></Picker.Item>);
+    }
+    render() {
+        return (
+            <View>
+                <Text style={styles.label}>
+                    Z waluty
+                    </Text>
+                <Picker
+                    selectedValue={this.props.currencyToConvert}
+                    onValueChange={itemValue =>
+                        this.props.setCurrencyToConvert(itemValue)}
+                    style={styles.selectInput}
+                >
+                    {
+                        this.displayCurrencies()
+                    }
+                </Picker>
+                <Text style={styles.label}>
+                    Na walutę
+                    </Text>
+                <Picker
+                    selectedValue={this.props.currencyTarget}
+                    onValueChange={itemValue => this.props.setCurrencyTarget(itemValue)}
+                    style={styles.selectInput}
+                >
+                    {
+                        this.displayCurrencies()
+                    }
+                </Picker>
+            </View>
+        )
+    }
+}
